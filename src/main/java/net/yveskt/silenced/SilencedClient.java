321e47e839064a95e2b7d9137cd0b13527d8dd1c
Yves.KT_ <yveskaiventamse200@gmail.com>
2024-01-01 03:55:16 +0800

    initial commit

---
+++ src/main/java/net/yveskt/silenced/SilencedClient.java
@@ -0,0 +1,10 @@
+package net.yveskt.silenced;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SilencedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
